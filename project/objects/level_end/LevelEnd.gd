0b9e04cd582a3743701beab07c4dc10fa73da31b
Devin Pentecost <devinpentecost@gmail.com>
2021-04-25 13:19:20 -0700

    Try

---
+++ project/objects/level_end/LevelEnd.gd
@@ -0,0 +1,15 @@
+extends Area
+signal level_ended
+
+onready var _player: Player = get_tree().get_nodes_in_group("player")[0]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _on_LevelEnd_body_entered(body):
+	if body == _player:
+		#Level is over!
+		print('Level has ended!')
+		$CollisionShape.disabled = true
+		emit_signal("level_ended")
